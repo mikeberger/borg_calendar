1fb20eb514aa6f98e8d88b29ee191f81c6e26f39
painter2 <devnull@localhost>
2004-12-17 14:03:44 +0000

    Now only testing CVS and ssh and Eclipse.

---
+++ BORGCalendarCommon/src/net/sf/borg/common/ui/JButtonKnowsBgColor.java
@@ -0,0 +1,23 @@
+/*
+ * Created on 17.12.2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package borg_src3.BORGCalendarCommon.src.net.sf.borg.common.ui;
+
+import javax.swing.JButton;
+
+/**
+ * This is not only CVS test, this class will be coded in future.
+ * 
+ * @author bsv
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class JButtonKnowsBgColor extends JButton {
+
+	public static void main(String[] args) {
+	}
+}
