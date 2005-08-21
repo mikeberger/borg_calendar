544546b9b9ce31dd5f42bce2d22a3a59f274f878
i_flem <devnull@localhost>
2005-08-21 19:36:01 +0000

    add a bit of caching - very simple - eliminates much of the remote traffic, but not all

--- BORGCalendarModel/src/net/sf/borg/model/db/remote/RemoteBeanDB.java
+++ BORGCalendarModel/src/net/sf/borg/model/db/remote/RemoteBeanDB.java
@@ -1,26 +1,27 @@
 /*
-This file is part of BORG.
+ This file is part of BORG.
  
-	BORG is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General public final synchronized License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+ BORG is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General public final synchronized License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
  
-	BORG is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General public final synchronized License for more details.
+ BORG is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General public final synchronized License for more details.
  
-	You should have received a copy of the GNU General public final synchronized License
-	along with BORG; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU General public final synchronized License
+ along with BORG; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  
-Copyright 2003 by Mike Berger
+ Copyright 2003 by Mike Berger
  */
 
 package net.sf.borg.model.db.remote;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 import net.sf.borg.common.util.XTree;
 import net.sf.borg.model.BorgOption;
@@ -31,125 +32,136 @@ import net.sf.borg.model.db.KeyedBean;
 /**
  * @author Mohan Embar
  */
-class RemoteBeanDB implements BeanDB
-{
+class RemoteBeanDB implements BeanDB {
+
+	private HashMap objectCache_; // the cache
+
 	// BeanDB overrides
-	public final synchronized Collection readAll() throws DBException, Exception
-	{
+	public final synchronized Collection readAll() throws DBException,
+			Exception {
 		return (Collection) call("readAll", null);
 	}
 
-	public final synchronized KeyedBean readObj(int key) throws DBException, Exception
-	{
-		return (KeyedBean) call("readObj", new Integer(key));
+	public final synchronized KeyedBean readObj(int key) throws DBException,
+			Exception {
+		Object o = objectCache_.get(new Integer(key));
+
+		if (o != null) {
+			KeyedBean r = (KeyedBean) o;
+			return r.copy();
+		}
+
+		KeyedBean bean = (KeyedBean) call("readObj", new Integer(key));
+
+		// put the bean in the cache
+
+		objectCache_.put(new Integer(key), bean);
+
+		return bean.copy();
+
 	}
 
-	public final synchronized KeyedBean newObj()
-	{
-		try
-		{
+	public final synchronized KeyedBean newObj() {
+		try {
 			return (KeyedBean) cls.newInstance();
+		} catch (Exception e) {
+			throw new AssertionError(e.getClass().getName() + ": "
+					+ e.getMessage());
 		}
-		catch (Exception e)
-		{
-			throw new AssertionError(e.getClass().getName()+": "+e.getMessage());
-		}
 	}
 
-	public final synchronized void addObj(KeyedBean bean, boolean crypt) throws DBException, Exception
-	{
+	public final synchronized void addObj(KeyedBean bean, boolean crypt)
+			throws DBException, Exception {
 		checkReadOnly();
-		call(
-			"addObj",
-			new IRemoteProxy.ComposedObject(bean, Boolean.valueOf(crypt)));
+		call("addObj", new IRemoteProxy.ComposedObject(bean, Boolean
+				.valueOf(crypt)));
+		objectCache_.put( new Integer(bean.getKey()), bean.copy() );
 	}
 
-	public final synchronized void updateObj(KeyedBean bean, boolean crypt) throws DBException, Exception
-	{
+	public final synchronized void updateObj(KeyedBean bean, boolean crypt)
+			throws DBException, Exception {
 		checkReadOnly();
-		call(
-			"updateObj",
-			new IRemoteProxy.ComposedObject(bean, Boolean.valueOf(crypt)));
+		call("updateObj", new IRemoteProxy.ComposedObject(bean, Boolean
+				.valueOf(crypt)));
 	}
 
-	public final synchronized void delete(int key) throws Exception
-	{
+	public final synchronized void delete(int key) throws Exception {
 		checkReadOnly();
+		objectCache_.remove( new Integer(key) );
 		call("delete", new Integer(key));
 	}
 
-	public final synchronized String getOption(String oname) throws Exception
-	{
+	public final synchronized String getOption(String oname) throws Exception {
 		return (String) call("getOption", oname);
 	}
 
-	public final synchronized void setOption(BorgOption option) throws Exception
-	{
+	public final synchronized void setOption(BorgOption option)
+			throws Exception {
 		checkReadOnly();
 		call("setOption", option);
 	}
 
-	public final synchronized Collection getOptions() throws Exception
-	{
+	public final synchronized Collection getOptions() throws Exception {
 		return (Collection) call("getOptions", null);
 	}
 
-	public final synchronized void close() throws Exception
-	{
+	public final synchronized void close() throws Exception {
 		// ignore this - we're sharing
 	}
 
-	public final synchronized int nextkey() throws Exception
-	{
+	public final synchronized int nextkey() throws Exception {
 		return ((Integer) call("nextkey", null)).intValue();
 	}
 
-	public final synchronized void sync() throws DBException
-	{
-		// ignore this - we're sharing
+	public final synchronized void sync() throws DBException {
+		objectCache_ = new HashMap();
 	}
-	
+
 	// protected //
-	protected Object call(String command, Object args) throws Exception
-	{
-		IRemoteProxy.Parms parms =
-			new IRemoteProxy.Parms(clsstr, command, args, user);
+	protected Object call(String command, Object args) throws Exception {
+		IRemoteProxy.Parms parms = new IRemoteProxy.Parms(clsstr, command,
+				args, user);
 		XTree xmlParms = XmlObjectHelper.toXml(parms);
 		String xmlstr = xmlParms.toString();
+		//System.out.println(xmlstr);
 		RemoteProxyHome home = RemoteProxyHome.getInstance();
-		String result =
-			home.getProxy(impl).execute(xmlstr, home.getProxyProvider());
+		String result = home.getProxy(impl).execute(xmlstr,
+				home.getProxyProvider());
+		//System.out.println(result);
 		XTree xmlResult = XTree.readFromBuffer(result);
-		
+
 		Object retval = XmlObjectHelper.fromXml(xmlResult);
 		if (retval instanceof Exception)
 			throw (Exception) retval;
-		
+
 		return retval;
 	}
-	
-	protected void checkReadOnly() throws DBException
-	{
+
+	protected void checkReadOnly() throws DBException {
 		if (readonly)
 			throw new DBException("Database is read-only",
 					DBException.RET_READ_ONLY);
 	}
-	
+
 	// package //
 	RemoteBeanDB(Class cls, String clsstr, String impl, boolean readonly,
-			String user)
-	{
+			String user) {
 		this.cls = cls;
 		this.clsstr = clsstr;
 		this.impl = impl;
 		this.readonly = readonly;
 		this.user = user;
+		objectCache_ = new HashMap();
 	}
-	
+
 	// private //
 	private Class cls;
+
 	private String clsstr;
+
 	private String impl;
+
 	private boolean readonly;
+
 	private String user;
 }
