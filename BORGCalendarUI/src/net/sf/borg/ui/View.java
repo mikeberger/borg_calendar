e3f522fa63587f7b13f94884af5ce6cf93f756e6
membar <devnull@localhost>
2005-09-08 13:02:08 +0000

    [Esc] = Dismiss button

--- BORGCalendarUI/src/net/sf/borg/ui/View.java
+++ BORGCalendarUI/src/net/sf/borg/ui/View.java
@@ -29,8 +29,13 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
 
 import net.sf.borg.common.util.PrefName;
 import net.sf.borg.common.util.Prefs;
@@ -126,11 +131,7 @@ abstract class View extends javax.swing.JFrame implements Model.Listener
     // protected //
     protected void setDismissButton(final JButton bn)
     {
-    	/*
-    	 * I had second thoughts about this. It allows you to dismiss
-    	 * the dialog boxes with [Escape], but this also happens when
-    	 * a menu is open.
-    	getRootPane()
+    	getLayeredPane()
 			.registerKeyboardAction
 			(
 				new ActionListener()
@@ -142,6 +143,5 @@ abstract class View extends javax.swing.JFrame implements Model.Listener
 				KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
 				JComponent.WHEN_IN_FOCUSED_WINDOW
 			);
-		*/
     }
 }
