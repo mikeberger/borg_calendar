8f1aefd2a8a77b945331691c488c5fc8b332f719
i_flem <devnull@localhost>
2005-10-11 23:06:56 +0000

    change GoTo to use JCalendar

--- BORGCalendarUI/src/net/sf/borg/ui/CalendarView.java
+++ BORGCalendarUI/src/net/sf/borg/ui/CalendarView.java
@@ -38,7 +38,6 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.URL;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Collection;
