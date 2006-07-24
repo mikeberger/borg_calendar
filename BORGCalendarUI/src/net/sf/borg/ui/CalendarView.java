0f6821da96729d1d8d514eb3d10c66bd34be81e0
i_flem <devnull@localhost>
2006-07-24 02:20:13 +0000

    dismiss main window when escape key pressed

--- BORGCalendarUI/src/net/sf/borg/ui/CalendarView.java
+++ BORGCalendarUI/src/net/sf/borg/ui/CalendarView.java
@@ -27,6 +27,9 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.net.URL;
@@ -42,10 +45,12 @@ import java.util.Vector;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.KeyStroke;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.ToolTipManager;
@@ -120,6 +125,19 @@ public class CalendarView extends View implements Prefs.Listener {
 		manageMySize(PrefName.CALVIEWSIZE);
 
 		ToolTipManager.sharedInstance().setDismissDelay(Integer.MAX_VALUE);
+		
+		getLayeredPane()
+		.registerKeyboardAction
+		(
+			new ActionListener()
+			{
+				public final void actionPerformed(ActionEvent e) {
+					exitForm(null);
+				}
+			},
+			KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+			JComponent.WHEN_IN_FOCUSED_WINDOW
+		);
 	}
 
 	private static class DayMouseListener implements MouseListener {
