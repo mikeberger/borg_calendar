cfcd3f8cd81576645bb8653bbd49afa11c930767
i_flem <devnull@localhost>
2007-02-13 03:29:51 +0000

    Run Jasper Reports through reflection so that the jasper software does not have to be included in the BORG installer - as this would make the installer too large.

--- BORGCalendarUI/src/net/sf/borg/ui/RunReport.java
+++ BORGCalendarUI/src/net/sf/borg/ui/RunReport.java
@@ -1,6 +1,7 @@
 package net.sf.borg.ui;
 
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,46 +9,55 @@ import java.util.Map;
 import net.sf.borg.common.util.Errmsg;
 import net.sf.borg.common.util.Resource;
 import net.sf.borg.model.db.jdbc.JdbcDB;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.view.JasperViewer;
 
 public class RunReport {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
+    /**
+         * @param args
+         */
+    public static void main(String[] args) throws Exception {
 
-		runReport("customer", new HashMap());
+	runReport("customer", new HashMap());
 
-	}
-	
-	public static void runReport(String name, Map parms) 
+    }
+
+    public static void runReport(String name, Map parms) {
+
+	if (parms == null)
+	    parms = new HashMap();
+	try {
+	    ClassLoader cl = ClassLoader.getSystemClassLoader();
+	    Class jprintclass = cl.loadClass("net.sf.jasperreports.engine.JasperPrint");
+	    Class jfillclass = cl.loadClass("net.sf.jasperreports.engine.JasperFillManager");
+	    Class jviewerclass = cl.loadClass("net.sf.jasperreports.view.JasperViewer");
+	    String resourcePath = "/reports/" + name + ".jasper";
+	    // Class.forName( "com.mysql.jdbc.Driver" );
+	    Connection conn = JdbcDB.getConnection();
+	    if (conn == null) {
+		Errmsg.notice(Resource.getPlainResourceString("no_reports"));
+		return;
+	    }
+	    InputStream is = RunReport.class.getResourceAsStream(resourcePath);
+	    Method fr = jfillclass.getMethod("fillReport", new Class[] {
+		    InputStream.class, Map.class, Connection.class });
+	    Object jasperprint = fr.invoke(null,
+		    new Object[] { is, parms, conn });
+	    // JasperPrint jasperPrint = JasperFillManager.fillReport(is,
+                // parms, conn);
+	    Method vr = jviewerclass.getMethod("viewReport", new Class[] {
+		    jprintclass, boolean.class });
+	    vr.invoke(null, new Object[] { jasperprint, new Boolean(false) });
+
+	    // JasperViewer.viewReport(jasperPrint,false);
+	    // conn.close();
+	} catch (ClassNotFoundException cnf)
 	{
-	    	if( parms == null ) parms = new HashMap();
-		try{
-			String resourcePath = "/reports/" + name + ".jasper";
-			//Class.forName( "com.mysql.jdbc.Driver" );
-			Connection conn = JdbcDB.getConnection();
-			if( conn == null )
-			{
-			    Errmsg.notice(Resource.getPlainResourceString("no_reports"));
-			    return;
-			}
-			InputStream is = RunReport.class.getResourceAsStream(resourcePath);
-			JasperPrint jasperPrint = JasperFillManager.fillReport(is, parms, conn);	 
-			JasperViewer.viewReport(jasperPrint,false);
-			conn.close();
-		}
-		catch(NoClassDefFoundError r)
-		{
-			Errmsg.notice(r.getMessage());
-		}
-		catch( Exception e)
-		{
-			Errmsg.errmsg(e);
-		}
+	    Errmsg.notice(Resource.getPlainResourceString("borg_jasp"));   
+	} catch (NoClassDefFoundError r) {
+	    Errmsg.notice(Resource.getPlainResourceString("borg_jasp"));
+	} catch (Exception e) {
+	    Errmsg.errmsg(e);
 	}
+    }
 
 }
