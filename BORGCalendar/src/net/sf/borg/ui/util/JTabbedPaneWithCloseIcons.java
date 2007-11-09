44ae337003a71437a50b6fe69385af3e7e27fd95
i_flem <devnull@localhost>
2007-11-09 22:11:31 +0000

    fix imports

--- BORGCalendar/src/net/sf/borg/ui/util/JTabbedPaneWithCloseIcons.java
+++ BORGCalendar/src/net/sf/borg/ui/util/JTabbedPaneWithCloseIcons.java
@@ -6,17 +6,12 @@ package net.sf.borg.ui.util;
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.Icon;
-import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
 import javax.swing.JTabbedPane;
 
-import net.sf.borg.common.Resource;
 import net.sf.borg.ui.DockableView;
 
 
