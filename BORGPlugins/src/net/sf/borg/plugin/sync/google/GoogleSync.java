b6fbac6a6b9c8e94db11d0f5f4ca7cb4efe10ea9
i_flem <devnull@localhost>
2010-07-21 21:32:42 +0000

    google sync - spawn thread for sync. lock UI with modal message window during sync.

--- BORGPlugins/src/net/sf/borg/plugin/sync/google/GoogleSync.java
+++ BORGPlugins/src/net/sf/borg/plugin/sync/google/GoogleSync.java
@@ -6,10 +6,13 @@ import java.util.Collection;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import javax.swing.SwingUtilities;
+
 import net.sf.borg.common.PrefName;
 import net.sf.borg.common.Prefs;
 import net.sf.borg.model.AppointmentModel;
 import net.sf.borg.model.entity.Appointment;
+import net.sf.borg.ui.util.ModalMessage;
 
 import com.google.gdata.client.calendar.CalendarQuery;
 import com.google.gdata.client.calendar.CalendarService;
@@ -26,100 +29,178 @@ public class GoogleSync {
 	static public PrefName SYNCPW = new PrefName("googlesync-pw", "");
 	static public PrefName SYNCPW2 = new PrefName("googlesync-pw2", "");
 
-	/**
-	 * do a sync and wipe. Since Googel cannot fully support everything in the
-	 * borg calendar model, just sync google changes to borg and then wipe and
-	 * update the entire google calendar with borg data. The changes that can be
-	 * made via google and then saved on borg are limited
-	 * 
-	 * currently, borg will not detect when appts are deleted from google. The appts will remain in borg
-	 * 
-	 * @throws Exception
-	 */
-	static public void sync() throws Exception {
+	static private class SyncThread extends Thread {
 
-		System.out.println("Google Sync Started");
+		private ModalMessage modalMessage = null;
+		
+		private void showMessage(String s, boolean unlock)
+		{
+			System.out.println(s);
+			final String fs = s;
+			final boolean funlock = unlock;
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					if (modalMessage.isShowing()) {
+						modalMessage.appendText(fs);
+						if( funlock == true)
+							modalMessage.setEnabled(true);
+					}
+				}
+			});
+		}
 
-		GoogleAppointmentAdapter ad = new GoogleAppointmentAdapter();
-
-		/**
-		 **************** only sync last 2 years ************8
-		 */
-		int years_to_sync = 2;
-		int syncFromYear = new GregorianCalendar().get(Calendar.YEAR)
-				- years_to_sync;
-
-		CalendarService myService = new CalendarService("BORG");
-		String user = Prefs.getPref(SYNCUSER);
-		String pw = GoogleSyncOptionsPanel.gep();
-		myService.setUserCredentials(user, pw);
-
-		URL eventUrl = new URL("http://www.google.com/calendar/feeds/" + user
-				+ "/private/full");
-
-		CalendarEventFeed batchDeleteRequest = new CalendarEventFeed();
-		int count = 1;
-
-		// get all new & changed appts
-		CalendarQuery query = new CalendarQuery(eventUrl);
-
-		// it is possible that the data may get large enough to exceed the
-		// amount of
-		// data that google will return in one query. If that ever happens, then
-		// paging can be done. For now, just up the max limit and hope.
-		query.setMaxResults(10000);
-		CalendarEventFeed myFeed = myService.query(query,
-				CalendarEventFeed.class);
-		List<CalendarEventEntry> entries = myFeed.getEntries();
-		System.out.println("Current Google Entries: " + entries.size());
-		for (CalendarEventEntry entry : entries) {
-
-			/*
-			 * borg sets sequence = 1 when sending to google, so anything with
-			 * sequence == 0 was created on google (noticed by trial/error) To
-			 * determine events changed on google, the publish (create) and edit
-			 * (update) dates are compared. Need to check for a difference of a
-			 * fw minutes between the times and the times are not the same for
-			 * newly insert appts (also found by trial/error) so - after sync,
-			 * don't update via google for 5 mins
-			 */
-			if (entry.getSequence() == 0
-					|| Math.abs(entry.getPublished().getValue()
-							- entry.getEdited().getValue()) > 5 * 60 * 1000) {
-				System.out.println("Needs Sync: " + entry.getIcalUID() + " "
-						+ entry.getSequence() + " "
-						+ entry.getTitle().getPlainText() + " "
-						+ entry.getPublished().toUiString() + " "
-						+ entry.getEdited().toUiString());
-				Appointment appt = ad.toBorg(entry);
-				AppointmentModel.getReference().saveAppt(appt);
+		@Override
+		public void run() {
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					if (modalMessage == null || !modalMessage.isShowing()) {
+						modalMessage = new ModalMessage("Google Sync Started",
+								false);
+						modalMessage.setVisible(true);
+					} else {
+						modalMessage.appendText("Google Sync Started");
+					}
+					modalMessage.setEnabled(false);
+					modalMessage.toFront();
+				}
+			});
+			
+			try {
+				doSync();
+			} catch (Exception e) {
+				e.printStackTrace();
+				this.showMessage(e.getLocalizedMessage(), true);
+				return;
 			}
 
-			// add every appt to a batch request to delete them all
-			// there is no simple way to empty the primary calendar
-			BatchUtils.setBatchId(entry, Integer.toString(count));
-			BatchUtils.setBatchOperationType(entry, BatchOperationType.DELETE);
-			batchDeleteRequest.getEntries().add(entry);
-			count++;
+			this.showMessage("Google Sync Finished", true);
 
 		}
-		System.out.println("Batch Count: " + count);
+		
+		private void doSync() throws Exception {
 
-		// only delete if there were appts in google
-		if (count > 1) {
-			// delete all events
-			myFeed = myService.getFeed(eventUrl, CalendarEventFeed.class);
-			Link batchLink = myFeed
-					.getLink(Link.Rel.FEED_BATCH, Link.Type.ATOM);
+			GoogleAppointmentAdapter ad = new GoogleAppointmentAdapter();
 
+			/**
+			 **************** only sync last 2 years ************8
+			 */
+			int years_to_sync = 2;
+			int syncFromYear = new GregorianCalendar().get(Calendar.YEAR)
+					- years_to_sync;
+
+			CalendarService myService = new CalendarService("BORG");
+			String user = Prefs.getPref(SYNCUSER);
+			String pw = GoogleSyncOptionsPanel.gep();
+			myService.setUserCredentials(user, pw);
+
+			URL eventUrl = new URL("http://www.google.com/calendar/feeds/" + user
+					+ "/private/full");
+
+			CalendarEventFeed batchDeleteRequest = new CalendarEventFeed();
+			int count = 1;
+
+			// get all new & changed appts
+			CalendarQuery query = new CalendarQuery(eventUrl);
+
+			// it is possible that the data may get large enough to exceed the
+			// amount of
+			// data that google will return in one query. If that ever happens, then
+			// paging can be done. For now, just up the max limit and hope.
+			query.setMaxResults(10000);
+			CalendarEventFeed myFeed = myService.query(query,
+					CalendarEventFeed.class);
+			List<CalendarEventEntry> entries = myFeed.getEntries();
+			this.showMessage("Current Google Entries: " + entries.size(), false);
+			for (CalendarEventEntry entry : entries) {
+
+				/*
+				 * borg sets sequence = 1 when sending to google, so anything with
+				 * sequence == 0 was created on google (noticed by trial/error) To
+				 * determine events changed on google, the publish (create) and edit
+				 * (update) dates are compared. Need to check for a difference of a
+				 * fw minutes between the times and the times are not the same for
+				 * newly insert appts (also found by trial/error) so - after sync,
+				 * don't update via google for 5 mins
+				 */
+				if (entry.getSequence() == 0
+						|| Math.abs(entry.getPublished().getValue()
+								- entry.getEdited().getValue()) > 5 * 60 * 1000) {
+					this.showMessage("Needs Sync: " + entry.getIcalUID() + " "
+							+ entry.getSequence() + " "
+							+ entry.getTitle().getPlainText() + " "
+							+ entry.getPublished().toUiString() + " "
+							+ entry.getEdited().toUiString(), false);
+					Appointment appt = ad.toBorg(entry);
+					AppointmentModel.getReference().saveAppt(appt);
+				}
+
+				// add every appt to a batch request to delete them all
+				// there is no simple way to empty the primary calendar
+				BatchUtils.setBatchId(entry, Integer.toString(count));
+				BatchUtils.setBatchOperationType(entry, BatchOperationType.DELETE);
+				batchDeleteRequest.getEntries().add(entry);
+				count++;
+
+			}
+
+			// only delete if there were appts in google
+			if (count > 1) {
+				// delete all events
+				myFeed = myService.getFeed(eventUrl, CalendarEventFeed.class);
+				Link batchLink = myFeed
+						.getLink(Link.Rel.FEED_BATCH, Link.Type.ATOM);
+
+				CalendarEventFeed batchResponse = myService.batch(new URL(batchLink
+						.getHref()), batchDeleteRequest);
+
+				// Ensure that all the operations were successful.
+				boolean isSuccess = true;
+				for (CalendarEventEntry entry : batchResponse.getEntries()) {
+
+					String batchId = BatchUtils.getBatchId(entry);
+					if (!BatchUtils.isSuccess(entry)) {
+						isSuccess = false;
+						BatchStatus status = BatchUtils.getBatchStatus(entry);
+						this.showMessage("\n" + batchId + " failed ("
+								+ status.getReason() + ") " + status.getContent(), false);
+					}
+				}
+				if (isSuccess) {
+					this.showMessage("Successfully processed all events via batch request.", false);
+				}
+			}
+
+			// now that borg has the latest synced view of all appts, send all borg
+			// events to google
+			CalendarEventFeed batchInsertRequest = new CalendarEventFeed();
+
+			GregorianCalendar cal = new GregorianCalendar();
+			Collection<Appointment> appts = AppointmentModel.getReference()
+					.getAllAppts();
+
+			count = 1;
+
+			for (Appointment appt : appts) {
+				cal.setTime(appt.getDate());
+				if (cal.get(Calendar.YEAR) >= syncFromYear) {
+					CalendarEventEntry ev = ad.fromBorg(appt);
+					BatchUtils.setBatchId(ev, Integer.toString(count));
+					BatchUtils.setBatchOperationType(ev, BatchOperationType.INSERT);
+					batchInsertRequest.getEntries().add(ev);
+					count++;
+				}
+			}
+
+			Link batchLink = myFeed.getLink(Link.Rel.FEED_BATCH, Link.Type.ATOM);
 			CalendarEventFeed batchResponse = myService.batch(new URL(batchLink
-					.getHref()), batchDeleteRequest);
+					.getHref()), batchInsertRequest);
 
 			// Ensure that all the operations were successful.
 			boolean isSuccess = true;
 			for (CalendarEventEntry entry : batchResponse.getEntries()) {
-
 				String batchId = BatchUtils.getBatchId(entry);
+				// System.out.println(entry.getId() + " "
+				// + entry.getTitle().getPlainText());
 				if (!BatchUtils.isSuccess(entry)) {
 					isSuccess = false;
 					BatchStatus status = BatchUtils.getBatchStatus(entry);
@@ -128,55 +209,27 @@ public class GoogleSync {
 				}
 			}
 			if (isSuccess) {
-				System.out
-						.println("Successfully processed all events via batch request.");
+				this.showMessage("Successfully processed all events via batch request.", false);
 			}
+
 		}
 
-		// now that borg has the latest synced view of all appts, send all borg events to google
-		CalendarEventFeed batchInsertRequest = new CalendarEventFeed();
+	}
 
-		GregorianCalendar cal = new GregorianCalendar();
-		Collection<Appointment> appts = AppointmentModel.getReference()
-				.getAllAppts();
-
-		count = 1;
-
-		for (Appointment appt : appts) {
-			cal.setTime(appt.getDate());
-			if (cal.get(Calendar.YEAR) >= syncFromYear) {
-				CalendarEventEntry ev = ad.fromBorg(appt);
-				BatchUtils.setBatchId(ev, Integer.toString(count));
-				BatchUtils.setBatchOperationType(ev, BatchOperationType.INSERT);
-				batchInsertRequest.getEntries().add(ev);
-				count++;
-			}
-		}
-
-		Link batchLink = myFeed.getLink(Link.Rel.FEED_BATCH, Link.Type.ATOM);
-		CalendarEventFeed batchResponse = myService.batch(new URL(batchLink
-				.getHref()), batchInsertRequest);
-
-		// Ensure that all the operations were successful.
-		boolean isSuccess = true;
-		for (CalendarEventEntry entry : batchResponse.getEntries()) {
-			String batchId = BatchUtils.getBatchId(entry);
-			// System.out.println(entry.getId() + " "
-			// + entry.getTitle().getPlainText());
-			if (!BatchUtils.isSuccess(entry)) {
-				isSuccess = false;
-				BatchStatus status = BatchUtils.getBatchStatus(entry);
-				System.out.println("\n" + batchId + " failed ("
-						+ status.getReason() + ") " + status.getContent());
-			}
-		}
-		if (isSuccess) {
-			System.out
-					.println("Successfully processed all events via batch request.");
-		}
-
-		System.out.println("Google Sync Finished");
-
+	
+	/**
+	 * do a sync and wipe. Since Googel cannot fully support everything in the
+	 * borg calendar model, just sync google changes to borg and then wipe and
+	 * update the entire google calendar with borg data. The changes that can be
+	 * made via google and then saved on borg are limited
+	 * 
+	 * currently, borg will not detect when appts are deleted from google. The
+	 * appts will remain in borg
+	 * 
+	 * @throws Exception
+	 */
+	static public void sync() throws Exception {
+		new SyncThread().start();
 	}
 
 }
