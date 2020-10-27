399f954aab8654926b62a91de2ad1533e0ba7a6e
Mike Berger <mike@mbcsoft.com>
2020-10-27 11:22:22 -0400

    add missing java module for jlink

--- BORGCalendar/install/buildjre.sh
+++ BORGCalendar/install/buildjre.sh
@@ -1,2 +1,2 @@
 
-${JAVA_HOME}/bin/jlink --module-path ${JAVA_HOME}/jmods --add-modules java.desktop,java.prefs,java.sql,java.xml,java.logging --output target/jre
+${JAVA_HOME}/bin/jlink --module-path ${JAVA_HOME}/jmods --add-modules java.desktop,java.prefs,java.sql,java.xml,java.logging,jdk.httpserver --output target/jre
