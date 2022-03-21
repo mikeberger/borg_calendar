ad6e4992423a4e391b6032c7d6d82d1a4bd71bc2
Mike Berger <mike@mbcsoft.com>
2022-03-21 18:05:09 -0400

    cleanup

--- BORGCalendar/swingui/src/main/java/net/sf/borg/ui/ResourceHelper.java
+++ BORGCalendar/swingui/src/main/java/net/sf/borg/ui/ResourceHelper.java
@@ -35,145 +35,30 @@ import javax.swing.*;
  */
 public class ResourceHelper {
 
-	/**
-	 * set the text and pnemonic for a JMenu
-	 * 
-	 * @param mnu
-	 *            the menu
-	 * @param resourceKey
-	 *            the resource string with optional pnemonic data
-	 */
+
 	public static void setText(JMenu mnu, String resourceKey) {
 		mnu.setText(Resource.getResourceString(resourceKey));
-		/*
-		ComponentParms parms = parseParms(resourceKey);
-		mnu.setText(parms.getText());
-		if (parms.getKeyEvent() != -1)
-			mnu.setMnemonic(parms.getKeyEvent());
-			*/
+
 	}
 
-	/**
-	 * set the text and pnemonic for a JMenuItem
-	 * 
-	 * @param mnuitm
-	 *            the JMenuItem
-	 * @param resourceKey
-	 *            the resource string with optional pnemonic data
-	 */
+
 	public static void setText(JMenuItem mnuitm, String resourceKey) {
 		mnuitm.setText(Resource.getResourceString(resourceKey));
-/*
-		ComponentParms parms = parseParms(resourceKey);
-		mnuitm.setText(parms.getText());
-		if (parms.getKeyEvent() != -1)
-			mnuitm.setMnemonic(parms.getKeyEvent());
-		if (parms.getKeyStroke() != null)
-			mnuitm.setAccelerator(parms.getKeyStroke());*/
+
 	}
 
-	/**
-	 * set the text and pnemonic for a button
-	 * 
-	 * @param button
-	 *            the button
-	 * @param resourceKey
-	 *            the resource string with optional pnemonic data
-	 */
+
 	public static void setText(AbstractButton button, String resourceKey) {
 		button.setText(Resource.getResourceString(resourceKey));
 
-/*		ComponentParms parms = parseParms(resourceKey);
-		button.setText(parms.getText());
-		if (parms.getKeyEvent() != -1)
-			button.setMnemonic(parms.getKeyEvent()); */
 	}
 
-	/**
-	 * set the text and pnemonic for a label
-	 * 
-	 * @param label
-	 *            the label
-	 * @param resourceKey
-	 *            the resource string with optional pnemonic data
-	 */
+
 	public static void setText(JLabel label, String resourceKey) {
 		label.setText(Resource.getResourceString(resourceKey));
 
-	/*	ComponentParms parms = parseParms(resourceKey);
-		label.setText(parms.getText());
-		if (parms.getKeyEvent() != -1)
-			label.setDisplayedMnemonic(parms.getKeyEvent()); */
 	}
 
-	
-	
-	/**
-	 * get a resource string and parse out the various parts - the text and the
-	 * keyboard shortcut into
-	 * 
-	 * @param resourceKey
-	 *            the resource key
-	 * 
-	 * @return the ComponentParms object
-	 */
-	/*
-	public static ComponentParms parseParms(String resourceKey) {
-		String parmsText = Resource.getRawResourceString(resourceKey);
 
-		String text = parmsText;
-		int mnemonic = -1;
-		KeyStroke accel = null;
-		int pos;
-		if ((pos = parmsText.indexOf('|')) != -1) {
-			text = parmsText.substring(0, pos);
-			String parmsTextRem = parmsText.substring(pos + 1);
-			String mnemonicText = parmsTextRem;
-
-			if ((pos = parmsTextRem.indexOf('|')) != -1) {
-				mnemonicText = parmsTextRem.substring(0, pos);
-				String accelText = parmsTextRem.substring(pos + 1);
-				accel = KeyStroke.getKeyStroke(accelText);
-			}
-
-			if (mnemonicText.length() > 0)
-				mnemonic = KeyStroke.getKeyStroke(mnemonicText).getKeyCode();
-		}
-		return new ComponentParms(text, mnemonic, accel);
-	}
-*/
-	/**
-	 * ComponentParms contains the text and keyboard shortcut info for a
-	 * resource string. Most resource strings do not have keyboard shortcut
-	 * info.
-	 */
-	/*
-	public static class ComponentParms {
-
-		public final int getKeyEvent() {
-			return keyEvent;
-		}
-
-		public final KeyStroke getKeyStroke() {
-			return keyStroke;
-		}
-
-		public final String getText() {
-			return text;
-		}
-
-		public ComponentParms(String text, int keyEvent, KeyStroke keyStroke) {
-			this.text = text;
-			this.keyEvent = keyEvent;
-			this.keyStroke = keyStroke;
-		}
-
-		private String text;
-
-		private int keyEvent;
-
-		private KeyStroke keyStroke;
-	}
-*/
 
 }
