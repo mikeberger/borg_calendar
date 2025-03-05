01c1c61402fb414c26e5bb1016763163233e34dd
Mike Berger <mike@mbcsoft.com>
2025-03-05 14:53:09 -0500

    new version, update 3pp, changelog, remove some jgoodies remnants

--- BORGCalendar/install/winpackage.bat
+++ BORGCalendar/install/winpackage.bat
@@ -1,1 +1,1 @@
-c:\java\jdk-21.0.2\bin\jpackage -t msi --app-version 1.10.05 -n BorgCalendar --vendor MBCSoft -i target\installer --main-class net.sf.borg.control.Borg --main-jar borg.jar --win-dir-chooser --win-menu --win-menu-group Borg --win-shortcut --icon borg.ico
+c:\java\jdk-21.0.2\bin\jpackage -t msi --app-version 1.10.06 -n BorgCalendar --vendor MBCSoft -i target\installer --main-class net.sf.borg.control.Borg --main-jar borg.jar --win-dir-chooser --win-menu --win-menu-group Borg --win-shortcut --icon borg.ico
