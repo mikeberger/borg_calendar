a623277e76a2756e7e7e1b2658f1b51e51ce79df
Mike Berger <mike@mbcsoft.com>
2025-11-25 19:58:57 -0500

    update version

--- BORGCalendar/install/linpackage.sh
+++ BORGCalendar/install/linpackage.sh
@@ -1,1 +1,1 @@
-$JAVA_HOME/bin/jpackage -t deb --app-version 1.10.06 -n BorgCalendar --vendor MBCSoft -i target/installer --main-class net.sf.borg.control.Borg --main-jar borg.jar --linux-menu-group Borg --linux-shortcut
+$JAVA_HOME/bin/jpackage -t deb --app-version 2.0.0 -n BorgCalendar --vendor MBCSoft -i target/installer --main-class net.sf.borg.control.Borg --main-jar borg.jar --linux-menu-group Borg --linux-shortcut
